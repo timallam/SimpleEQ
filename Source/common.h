d2633f03015244ddbb52b0a86764d2b1fc81945d
timallam <timur.allamiyarov@gmail.com>
2021-07-04 11:59:36 +1000

    First commit

---
+++ Source/common.h
@@ -0,0 +1,23 @@
+//
+//  common.h
+//  SimpleEQ
+//
+//  Created by Tim on 3/7/21.
+//
+
+#ifndef common_h
+#define common_h
+
+namespace common_decl
+{
+    const std::string LOW_CUT_FREQUENCY  = "Low Cut Freq";
+    const std::string HIGH_CUT_FREQUENCY = "High Cut Freq";
+    const std::string PEAK_FREQUENCY     = "Peak Freq";
+    const std::string PEAK_GAIN          = "Peak Gain";
+    const std::string PEAK_QUALITY       = "Peak Quality";
+
+    const std::string LOW_CUT_SLOPE      = "Low Cut Slope";
+    const std::string HIGH_CUT_SLOPE     = "High Cut Slope";
+}
+
+#endif /* common_h */
